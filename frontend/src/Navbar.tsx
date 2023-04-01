20a3678de9c55f420cb4ace25baccbed228c23cd
jessiann22 <Jessicaa4755@gmail.com>
2023-04-01 13:07:05 -0600

    it is finished

---
+++ frontend/src/Navbar.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Navbar.css';
+
+function Navbar() {
+  return (
+    <div className="navbar-container">
+      <nav className="navbar-links">
+        <Link to="/" className="navbar-link">
+          Home
+        </Link>
+        <Link to="/Podcast" className="navbar-link">
+          Podcast
+        </Link>
+        <Link to="/MovieCollection" className="navbar-link">
+          Movie Collection
+        </Link>
+      </nav>
+    </div>
+  );
+}
+
+export default Navbar;
